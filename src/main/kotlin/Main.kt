10308c3c408293e10c3fa1272910cea1c853b325
lekishonsheila <lekishonsheila@gmail.com>
2023-02-15 08:38:58 +0300

    first commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,33 @@
+fun main() {
+    val school="AkiraChix"
+    println(school[1])
+    println(school[3])
+    println(school[4])
+
+    val name= "Sheila"
+    val age= "19"
+    val sentence="Hi,my name is $name and i am $age years old"
+    println(sentence)
+
+    val lenght= 40.5
+    println("the lenght is 40.5")
+
+    val name1= "sheila"
+    val name2= "sheila"
+    if (name1==name2){
+        println("That's me")
+    }
+    else{
+        println("I don't know who that is")
+    }
+
+
+
+
+
+
+
+
+
+
+}
